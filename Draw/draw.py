e9bced498c2998effc3d40e94f0c51be49ef5ec5
Koyama-Yu <xiaoshanyu427@gmail.com>
2024-02-02 14:05:18 +0900

    draw new graph

--- Draw/draw.py
+++ Draw/draw.py
@@ -6,16 +6,20 @@ from matplotlib import pyplot as plt
 import csv
 
 episode_num = 100
-fname_eps = 'OptimalActionProbability_Ave_Epsilon01.npy'   #change when you need
+fname_eps_fix = 'OptimalActionProbability_Ave_Epsilon01.npy'   #change when you need
+fname_eps_ren = 'OptimalActionProbability_Ave_Epsilon_Co0.999.npy'   #change when you need
 fname_la = 'OptimalActionProbability_Ave_LA_theta1.npy'   #change when you need
-episodes_eps = np.load(fname_eps)
+
+episodes_eps_fix = np.load(fname_eps_fix)
+episodes_eps_ren = np.load(fname_eps_ren)
 episodes_la = np.load(fname_la)
 #firststep = np.zeros(firststep_num)
 #ave = np.mean(steps[101:])
 
 plt.rcParams['xtick.direction'] = 'in'
 plt.rcParams['ytick.direction'] = 'in'
-plt.plot(np.arange(0, episode_num), episodes_eps, ls='--', label='ε-greedy', color='dodgerblue')
+plt.plot(np.arange(0, episode_num), episodes_eps_fix, ls='--', label='ε-greedy (ε fixed)', color='dodgerblue')
+plt.plot(np.arange(0, episode_num), episodes_eps_ren, ls=':', label='ε-greedy (ε renewed)', color='dodgerblue')
 plt.plot(np.arange(0, episode_num), episodes_la, ls='-',label='LQ', color='dodgerblue')
 #plt.plot(np.arange(0, step_num - 200), steps1[:300], color = 'dodgerblue')
 #plt.plot(np.arange(0, step_num - 200), steps2[:300], color = 'salmon')
@@ -25,5 +29,5 @@ plt.plot(np.arange(0, episode_num), episodes_la, ls='-',label='LQ', color='dodge
 plt.xlabel("episodes")
 plt.ylabel("optimal action selection rate")
 plt.legend()
-plt.savefig('compare_each_method.png')   #change when you need
+plt.savefig('compare_each_method_add_epsilonren.png')   #change when you need
 plt.show()
